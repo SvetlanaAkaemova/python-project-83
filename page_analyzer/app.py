9c15dd436c2c06eb5be87fa2eb28f009768cdd60
svetlana_akaemova <akaemova.sv@mail.ru>
2023-01-30 22:28:38 +0100

    add url check

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,6 @@
 import psycopg2
 import os
+import requests
 import datetime
 import validators
 from flask import Flask, request, url_for, get_flashed_messages, flash, redirect, render_template
@@ -30,8 +31,7 @@ def get_id(db, dt, name):
 
 @app.route('/')
 def start():
-    messages = get_flashed_messages(with_categories=True)
-    return render_template('start.html', messages=messages)
+    return render_template('start.html')
 
 
 @app.post('/urls')
@@ -40,10 +40,10 @@ def post_url():
     if url == '':
         flash("Некорректный URL", "alert alert-danger")
         flash("URL обязателен", "alert alert-danger")
-        return redirect(url_for('start'))
+        return redirect(url_for('urls_get'))
     if not validators.url(url):
         flash("Некорректный URL", "alert alert-danger")
-        return redirect(url_for('start'))
+        return redirect(url_for('urls_get'))
     url_id_in_db = get_id(DATABASE_URL, 'urls', url)
     if url_id_in_db is None:
         conn = psycopg2.connect(DATABASE_URL)
@@ -73,13 +73,13 @@ def url_added(id):
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
     cur.execute(
-        "SELECT id, created_at FROM url_checks WHERE url_id = {0}".format(id)
+        "SELECT id, created_at, status_code FROM url_checks WHERE url_id = {0} ORDER BY id DESC".format(id)
     )
     rows = cur.fetchall()
     list_of_rows = list(map(list, rows))
     check_list = []
     for row in list_of_rows:
-        check_list.append({'id': row[0], 'created_at': row[1].date(), 'status_code': 0, 'h1': '', 'title': '', 'description': ''})
+        check_list.append({'id': row[0], 'created_at': row[1].date(), 'status_code': row[2], 'h1': '', 'title': '', 'description': ''})
     conn.close()
     return render_template(
         'new_site.html',
@@ -93,18 +93,27 @@ def url_added(id):
 
 @app.get('/urls')
 def urls_get():
+    messages = get_flashed_messages(with_categories=True)
+    if messages:
+        return render_template(
+            'start.html',
+            messages=messages)
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
     cur.execute(
-        "SELECT urls.id, urls.name, MAX(url_checks.created_at) AS max FROM urls LEFT JOIN url_checks ON urls.id = url_checks.url_id GROUP BY urls.id ORDER BY urls.id DESC;"
+        "SELECT DISTINCT ON (urls.id) urls.id, urls.name, MAX(url_checks.created_at), url_checks.status_code FROM urls LEFT JOIN url_checks ON urls.id = url_checks.url_id GROUP BY urls.id, url_checks.status_code ORDER BY urls.id DESC"
     )
     rows = cur.fetchall()
     list_of_rows = list(map(list, rows))
     urls_list = []
     for row in list_of_rows:
+        if row[3] is None:
+            row[3] = ''
         if row[2] is None:
             row[2] = ''
-        urls_list.append({'id': row[0], 'name': row[1], 'check_data': row[2], 'response_code': 0})
+        else:
+            row[2] = row[2].date()
+        urls_list.append({'id': row[0], 'name': row[1], 'check_data': row[2], 'response_code': row[3]})
     conn.close()
     return render_template(
         'site_list.html',
@@ -116,9 +125,22 @@ def urls_get():
 def id_check(id):
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
+    cur.execute(
+        "SELECT name FROM urls WHERE id = {0}".format(id)
+    )
+    result = cur.fetchall()
+    conn.close()
+    url_name = [x[0] for x in result][0]
+    response = requests.get(url_name)
+    status_code = response.status_code
+    if status_code != 200:
+        flash("Произошла ошибка при проверке", "alert alert-danger")
+        return redirect(url_for('url_added', id=id))
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
     date = datetime.date.today()
     cur.execute(
-        "INSERT INTO url_checks (url_id, created_at) VALUES ('{0}', '{1}')".format(id, date)
+        "INSERT INTO url_checks (url_id, created_at, status_code) VALUES ('{0}', '{1}', {2})".format(id, date, status_code)
     )
     conn.commit()
     conn.close()
