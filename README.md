a5d38f897c1efb91b7fe021a9e3338ca4eccf7be
svetlana_akaemova <akaemova.sv@mail.ru>
2023-02-03 23:31:24 +0100

    fixed files before check 1

--- README.md
+++ README.md
@@ -3,4 +3,9 @@
 
 [![Actions Status](https://github.com/SvetlanaAkaemova/python-project-83/workflows/my_check/badge.svg)](https://github.com/SvetlanaAkaemova/python-project-83/actions)
 
+[![Maintainability](https://api.codeclimate.com/v1/badges/8afeea4d14aef2bd9ab5/maintainability)](https://codeclimate.com/github/SvetlanaAkaemova/python-project-83/maintainability)
+
+
+Page Analyzer is a web application that checks web pages for SEO suitability
+
 https://python-project-83-production-9aea.up.railway.app
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,6 +6,7 @@ import datetime
 import validators
 from flask import Flask, request, url_for, get_flashed_messages, flash, redirect, render_template
 from dotenv import load_dotenv
+from psycopg2 import OperationalError
 
 
 app = Flask(__name__)
@@ -14,6 +15,15 @@ app = Flask(__name__)
 load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
+
+
+def create_connection(db):
+    try:
+        connection = psycopg2.connect(db)
+
+    except OperationalError as e:
+        print(f"The error '{e}' occured")
+    return connection
 
 
 def get_content_of_site(url):
@@ -36,7 +46,7 @@ def get_content_of_site(url):
 
 
 def get_id(db, dt, name):
-    conn = psycopg2.connect(db)
+    conn = create_connection(db)
     cur = conn.cursor()
     cur.execute(
         "SELECT id FROM {0} WHERE name = '{1}'".format(dt, name)
@@ -50,23 +60,32 @@ def get_id(db, dt, name):
 
 
 @app.route('/')
-def start():
-    return render_template('start.html')
+def index():
+    return render_template('index.html')
 
 
 @app.post('/urls')
 def post_url():
     url = request.form.get('url')
+    messages = get_flashed_messages(with_categories=True)
     if url == '':
         flash("Некорректный URL", "alert alert-danger")
         flash("URL обязателен", "alert alert-danger")
-        return redirect(url_for('urls_get'))
-    if not validators.url(url):
+        return render_template(
+            'index.html',
+            url_input=url,
+            messages=messages
+        )
+    if not validators.url(url) and url != '':
         flash("Некорректный URL", "alert alert-danger")
-        return redirect(url_for('urls_get'))
+        return render_template(
+            'index.html',
+            url_input=url,
+            messages=messages
+        )
     url_id_in_db = get_id(DATABASE_URL, 'urls', url)
     if url_id_in_db is None:
-        conn = psycopg2.connect(DATABASE_URL)
+        conn = create_connection(DATABASE_URL)
         cur = conn.cursor()
         date = datetime.date.today()
         cur.execute(
@@ -84,25 +103,27 @@ def post_url():
 
 @app.get('/urls/<id>')
 def url_added(id):
-    conn = psycopg2.connect(DATABASE_URL)
+    conn = create_connection(DATABASE_URL)
     cur = conn.cursor()
     messages = get_flashed_messages(with_categories=True)
-    cur.execute("SELECT name, created_at FROM urls WHERE id = {0}".format(id))
+    cur.execute(
+        "SELECT name, created_at FROM urls WHERE id = {0}".format(id)
+    )
     url_name, url_created_at = cur.fetchone()
     conn.close()
-    conn = psycopg2.connect(DATABASE_URL)
+    conn = create_connection(DATABASE_URL)
     cur = conn.cursor()
     cur.execute(
         "SELECT id, created_at, status_code, h1, title, description FROM url_checks WHERE url_id = {0} ORDER BY id DESC".format(id)
     )
     rows = cur.fetchall()
     list_of_rows = list(map(list, rows))
+    conn.close()
     check_list = []
     for row in list_of_rows:
         check_list.append({'id': row[0], 'created_at': row[1].date(), 'status_code': row[2], 'h1': row[3], 'title': row[4], 'description': row[5]})
-    conn.close()
     return render_template(
-        'new_site.html',
+        'site.html',
         messages=messages,
         url_name=url_name,
         url_id=id,
@@ -113,18 +134,14 @@ def url_added(id):
 
 @app.get('/urls')
 def urls_get():
-    messages = get_flashed_messages(with_categories=True)
-    if messages:
-        return render_template(
-            'start.html',
-            messages=messages)
-    conn = psycopg2.connect(DATABASE_URL)
+    conn = create_connection(DATABASE_URL)
     cur = conn.cursor()
     cur.execute(
         "SELECT DISTINCT ON (urls.id) urls.id, urls.name, MAX(url_checks.created_at), url_checks.status_code FROM urls LEFT JOIN url_checks ON urls.id = url_checks.url_id GROUP BY urls.id, url_checks.status_code ORDER BY urls.id DESC"
     )
     rows = cur.fetchall()
     list_of_rows = list(map(list, rows))
+    conn.close()
     urls_list = []
     for row in list_of_rows:
         if row[3] is None:
@@ -134,22 +151,22 @@ def urls_get():
         else:
             row[2] = row[2].date()
         urls_list.append({'id': row[0], 'name': row[1], 'check_data': row[2], 'response_code': row[3]})
-    conn.close()
     return render_template(
-        'site_list.html',
+        'sites.html',
         urls_list=urls_list
     )
 
 
 @app.route('/urls/<id>/checks', methods=['POST'])
 def id_check(id):
-    conn = psycopg2.connect(DATABASE_URL)
+    conn = create_connection(DATABASE_URL)
     cur = conn.cursor()
     cur.execute(
         "SELECT name FROM urls WHERE id = {0}".format(id)
     )
     result = cur.fetchall()
     conn.close()
+
     url_name = [x[0] for x in result][0]
     response = requests.get(url_name)
     status_code = response.status_code
@@ -157,7 +174,7 @@ def id_check(id):
         flash("Произошла ошибка при проверке", "alert alert-danger")
         return redirect(url_for('url_added', id=id))
     h1, title, content = get_content_of_site(url_name)
-    conn = psycopg2.connect(DATABASE_URL)
+    conn = create_connection(DATABASE_URL)
     cur = conn.cursor()
     date = datetime.date.today()
     cur.execute(
