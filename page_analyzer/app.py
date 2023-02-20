71ff85cff4e867e061e4232e10ce0bcd2bb74b01
svetlana_akaemova <akaemova.sv@mail.ru>
2023-02-20 21:46:29 +0100

    rename parameter in function

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -23,8 +23,8 @@ def get_connection():
     return psycopg2.connect(DATABASE_URL)
 
 
-def get_content_of_page(page_text):
-    soup = bs4.BeautifulSoup(page_text, 'html.parser')
+def get_content_of_page(page_data):
+    soup = bs4.BeautifulSoup(page_data, 'html.parser')
     h1 = soup.find('h1').get_text() if soup.find('h1') else ''
     title = soup.find('title').get_text() if soup.find('title') else ''
     meta = soup.find(
